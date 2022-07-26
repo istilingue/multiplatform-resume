48e1ec01596106bd62010b3de7b8e66790edc262
Lucas Stirling <stirling.lucas@gmail.com>
2022-07-26 14:48:40 -0300

    feat: finalizano a tela de seleção de plataforma

---
+++ src/app/models/resume.ts
@@ -0,0 +1,40 @@
+export const MOCK_RESUME: any = {
+  platforms : ["Apple", "Microsoft", "Google", "Amazon", "Facebook"],
+  profile : {
+    fullName: "Lucas Stirling de Albuquerque",
+    birthDate:"02/09/1996",
+    email:"stirling.lucas@gmail.com",
+    city:"Recife",
+    state:"PE",
+    cellphone: "(81) 99783-1767",
+    github: "istilingue",
+    currentRole:"Desenvolvedor Front-End"
+  },
+  education : [
+    {
+      from:'',
+      to:'',
+      company:'',
+      responsibility:'',
+      role:''
+    }
+
+  ],
+  languages : [{
+    language: "English",
+    level: "Fluente"
+  },
+  {
+    language: "Português",
+    level: "Nativo"
+  }],
+
+  programming : ["HTML",
+    "CSS",
+    "JS/TS",
+    "React",
+    "Angular"],
+
+  aboutMe : ["Sou um programador obstinado, sempre inquieto para aprender mais e faminto de oportunidades. Gosto de desafios ........"]
+
+}
--- src/app/select-platform/select-platform.component.ts
+++ src/app/select-platform/select-platform.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MOCK_RESUME } from '../models/resume';
+import { SelectPlatformService } from '../services/select-platform.service';
 
 @Component({
   selector: 'app-select-platform',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-platform.component.scss']
 })
 export class SelectPlatformComponent implements OnInit {
+  platforms = MOCK_RESUME.platforms;
+  url = 'localhost:4200'
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  redirect(item: string) {
+    let urlPlatform = item.charAt(0).toLowerCase() + item.slice(1);
+    this.router.navigate([`/${urlPlatform}`]);
+
+  }
 }
